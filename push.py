395b97d9b05b66052541f619864603095d247b23
unknown <pratham.extra.01@gmail.com>
2026-05-05 01:12:15 +0530

    Update landing page

--- push.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-repo_path = r"C:\Users\Pratham G\Desktop\Programming\Dhir Elevative\Elevative Bundle"
-
-subprocess.run("git add .", cwd=repo_path, shell=True)
-subprocess.run('git commit -m "Update landing page"', cwd=repo_path, shell=True)
-subprocess.run("git push", cwd=repo_path, shell=True)
