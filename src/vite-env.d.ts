c4a1048d8f92a8c56d690523218997a1fdb4ac98
unknown <pratham.extra.01@gmail.com>
2026-05-05 01:04:54 +0530

    Update landing page

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,16 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module "*.png" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.jpg" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.jpeg" {
-  const src: string;
-  export default src;
-}
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
